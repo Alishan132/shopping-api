6daa87c9b96307d90c4b7da019f0a5fd82ccbc64
Alishan Atashev <alishanatasheva@gmail.com>
2023-06-16 20:56:20 +0600

    Initial Setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
+const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// MIDDLEWARES
+app.use(express.json());
+
+// ROUTES
+app.use("/api/users", userRoute);
+app.use("/api/auth");
+app.use("/api/users");
+app.use("/api/products");
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Backend server is running");
+});
